bca3060e804e5c36833f4a2e6317237e6f84300b
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-14 03:53:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-var kittens = ["Milo", "Otis","Garfield"]; //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
-function destructivelyAppendKitten(name){
+function destructivelyAppendKitten(name) {
   kittens.push(name);
+  return kittens;
 }
-function destructivelyPrependKitten(name){
+
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
+  return kittens;
 }
-function destructivelyRemoveLastKitten(){
+
+function destructivelyRemoveLastKitten() {
   kittens.pop();
+  return kittens;
 }
-function destructivelyRemoveFirstKitten(){
+
+function destructivelyRemoveFirstKitten() {
   kittens.shift();
+  return kittens;
 }
-function appendKitten(name){
+
+function appendKitten(name) {
   return [...kittens, name];
 }
-function prependKitten(name){
-  return [name, ...kittens];
+
+function prependKitten(name) {
+  return [name, ...kittens]
 }
-function removeLastKitten(){
-  return kittens.slice(0, kittens.length - 1);
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1);
 }
-function removeFirstKitten(){
+
+function removeFirstKitten() {
   return kittens.slice(1);
 }
