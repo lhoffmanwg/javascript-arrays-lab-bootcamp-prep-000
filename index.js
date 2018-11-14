97566cd94259aa8b4058729dc44b0f9208fb71ad
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-14 04:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,9 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name];
 }
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+  
+  
